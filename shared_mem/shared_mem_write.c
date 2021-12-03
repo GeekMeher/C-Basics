b11abfed7593a32dcfcafe2bf620831527d1bf62
meiheru <yarravarapu.meiheru@ltts.com>
2021-12-03 11:43:01 +0530

    shared mem simple eg

---
+++ shared_mem/shared_mem_write.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+int 
+main ()
+{
+	// ftok to generate unique key
+	key_t key = ftok("shmfile",65);
+	// shmget returns an identifier in shmid
+	int shmid = shmget(key,1024,0666|IPC_CREAT);
+
+	// shmat to attach to shared memory
+    	char *str = (char*) shmat(shmid,(void*)0,0);
+    	strcpy (str,"Testing write semphores");
+
+    	printf("Data written in memory: %s\n",str);
+
+    	//detach from shared memory
+    	shmdt(str);
+	return 0;
+}
